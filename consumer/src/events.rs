c1570167cc98a10f2392bc6e31ecd9eb78cba480
imabdulbasit <baronbasit@gmail.com>
2023-06-02 01:33:13 +0500

    :/

--- consumer/src/events.rs
+++ consumer/src/events.rs
@@ -53,7 +53,7 @@ fn send_invite_email(
     let html: String = tera.render("send_invite.html", &context)?;
 
     let email = Message::builder()
-        .from(format!("Holaplex Support<{source_email}>").parse()?)
+        .from(format!("Holaplex Support <{source_email}>").parse()?)
         .to(email.parse().unwrap())
         .subject(format!("You have been invited to join {organization}"))
         .header(ContentType::TEXT_HTML)
